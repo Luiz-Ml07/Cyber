7abf3811be1ef37dde5b225b41205f044f0c7c4f
Luiz-Ml07 <joaopedrobut12@gmail.com>
2026-04-18 15:13:20 -0300

    Delete src directory

--- src/javascript/script.js
+++
@@ -1,6 +0,0 @@
-const mobileBtn = document.getElementById('mobile_btn');
-const mobileMenu = document.getElementById('mobile_menu');
-
-mobileBtn.addEventListener('click', function () {
-    mobileMenu.classList.toggle('active');
-});
